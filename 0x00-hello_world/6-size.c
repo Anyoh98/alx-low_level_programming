08a45f7c89b854775b2bb9bef33c9e4ecb004be1
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-20 16:33:38 -0700

    Hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,6 +5,7 @@
  */
 int main(void)
 {
+printf("Size of a char: %d byte(s)\n", sizeof(char));
 printf("Size of an int: %d byte(s)\n", sizeof(int));
 prinf("Size of a long int: %d byte(s)\n", sizeof(long int));
 prinf("Size of a long long int: %d byte(s)\n", sizeof(long long int));

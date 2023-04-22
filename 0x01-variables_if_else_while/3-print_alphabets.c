b55da5b2ca2646cceb9c9ced99efee168a8918e8
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-21 18:08:45 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: get the last digit of a number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+	}
+	while (m <= 98)
+	{
+		putchar(m);
+	}
+	putchar('\n');
+	return (0);
+}

ba6ae7a2ed36381f2fb7c29ad6081246193d6061
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-24 08:18:10 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry poitn
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		putchar((n % 10) + '0');
+	}
+	for (n = 97;  n <= 103; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}

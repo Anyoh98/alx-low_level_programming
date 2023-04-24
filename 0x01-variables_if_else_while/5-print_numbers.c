8a67a815df7ab56295331d27dd7d0d89b1361acd
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-24 02:31:51 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * return: always (0)
+ */
+
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb <10; numb++)
+	{
+		printf(numb'\n');
+	}
+	return 0;
+}

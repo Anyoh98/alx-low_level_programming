3fe49cd9d694682a37a0939c4e786b1f6046011d
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-24 01:44:03 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdioh>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (sucess)
+ */
+
+int main(void)
+{
+	int lowerCase = 97
+
+	for(lowerCase = 97; lowerCase <= 122; lowerCase++)
+	{
+		if(lowerCase != 101 && lowerCase != 122)
+		{
+			putchar(lowerCase);
+		}
+		putchar('\n');
+		Return (0);
+	}
+}

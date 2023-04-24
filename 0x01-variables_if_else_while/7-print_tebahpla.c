a7f7ba3d6758bd98834225904e4ccc69510c9033
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-24 04:04:31 -0700

    7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
 
 /**
- * main: Print lower case alphabet in reverse
- *
- * Return: Always 0 
+ * main: Entry point
+ * Description:Print lower case alphabet in reverse
+ * Return: Always 0
  */
 
-int main (void)
+int main(void)
 {
 	int lowerCase;
 
-	for (lowerCase = 123; lowerCase > 97; lowerCase--)
+	for (lowerCase = 122; lowerCase > 97; lowerCase--)
 	{
 		putchar(lowerCase);
-	}	
+	}
 	putchar('\n');
 	return (0);
 }

4c9fc22c4c0eb7a342b8302daa2c2f65adab7d23
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-25 15:29:23 -0700

    2-alphabet_x10

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,17 +8,17 @@
 
 void print_alphabet_x10(void)
 {
-        int i;
+       	int i;
 	int a;
-        i=0;
+	i = 0;
 
-        while (i < 10)
-        {
-                for (a = 97; a < 123; a++)
-                {
-                        _putchar(a);
-                }
-        _putchar('\n');
-	i++;
-        }
+	while (i < 10)
+	{
+		for (a = 97; a < 123; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+		i++;
+	}
 }

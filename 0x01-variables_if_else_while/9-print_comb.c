c5005133f29da3e007113c8b3778303c5331261c
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-26 06:16:03 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,18 +7,18 @@
 
 int main(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i < 10; i++)
-    {
-        putchar(i + 48);
-        if (i < 9)
-        {
-            continue;
-        }
-	putchar(44);
-	putchar(32);
-    }
-    putchar('\n');
-    return (0);
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + 48);
+		if (i = 9)
+		{
+			continue;
+		}
+		putchar(44);
+		putchar(32);
+	}
+	putchar('\n');
+	return (0);
 }

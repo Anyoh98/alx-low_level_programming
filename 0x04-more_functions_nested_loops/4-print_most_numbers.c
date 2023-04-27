bdbc28a412a172987959503b77c3afc75c50db3b
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-27 13:05:27 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,14 +7,15 @@
 
 void print_most_numbers(void)
 {
-	int i;
+	int i = 48;
 
-	for (i = 48; i <= 57; i++)
+	while (i > 59)
 	{
-		if (i != 50 || i != 52)
+		if (i == 50 || i == 52)
 		{
-			_putchar(i);
+			continue;
 		}
+		_putchar(i);
 		i++;
 	}
 	_putchar('\n');

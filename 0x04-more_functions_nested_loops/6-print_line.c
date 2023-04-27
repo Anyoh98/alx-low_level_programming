a380b5df7f5128cd0e1fafe9562ccf9a10970ab4
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-27 12:39:48 -0700

    3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_line - Function draws a safe line
+ * @n: number of times _ should be printed.
+ * Return: no return value.
+ */
+
+void print_line(int n)
+{
+	
+}

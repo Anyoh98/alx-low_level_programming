2da61186aa9148007cd45456c2df685402f97e61
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-27 11:08:07 -0700

    2-mul.c

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - Function that multiplies two intergers
+ * @a: digit to be multiplied.
+ * @b: Digit to be multiplied.
+ * Return: 0 always (success)
+ */
+
+int mul(int a, int b)
+{
+	int multiplication = a * b;
+
+	return (multiplication);
+}

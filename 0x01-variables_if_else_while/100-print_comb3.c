7e5814fe442e556fbf25081372464673f1a6cb51
Anyoh98 <fonachuanyoh@yahoo.com>
2023-04-28 03:20:44 -0700

    100-print_comb3.c

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,28 +7,27 @@
 
 int main(void)
 {
-    int n;
-    int m;
+	int n;
+       	int m;
 
-    for (n = 48; n < 58; n++)
-    {
-        for (m = n + 1; m < 58; m++)
-        {
-             if (m != n )
-            {
-                putchar(n);
-                putchar(m);
-            }
-            if (n == 56 && m == 57)
-            {
-                continue;
-            }
-            putchar(44);
-            putchar(32);
-        }
-    }
-    putchar('\n');
-    return (0);   
-    
+       	for (n = 48; n < 58; n++)
+	{
+		for (m = n + 1; m < 58; m++)
+		{
+		       	if (m != n )
+			{
+				putchar(n);
+				putchar(m);
+			}
+			if (n == 56 && m == 57)
+			{
+				continue;
+			}
+			putchar(44);
+			putchar(32);
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 

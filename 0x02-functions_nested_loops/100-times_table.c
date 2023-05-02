30c451047a9b96aa91faf83dc36e068802679260
Anyoh98 <fonachuanyoh@yahoo.com>
2023-05-02 03:55:50 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -17,7 +17,7 @@ void print_times_table(int n)
 			for (j = 0; j <= n; j++)
 			{
 				result = i * j;
-				if (j == 0 || i == 0)
+				if (j == 0)
 				{
 					_putchar(result + 48);
 				}
@@ -29,7 +29,7 @@ void print_times_table(int n)
 					_putchar(result / 10 + '0');
 					_putchar(result % 10 + 48);
 				}
-				if (result < 10 && j != 0 && i != 0)
+				if (result < 10 && j != 0)
 				{
 					_putchar(',');
 					_putchar(' ');

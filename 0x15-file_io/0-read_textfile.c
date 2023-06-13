f387e3d828dafc7df3c1167d4ef3aa6de174e2fe
Anyoh98 <fonachuanyoh@yahoo.com>
2023-06-13 03:50:49 -0700

    mine

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,27 +2,38 @@
 #include <stdlib.h>
 
 /**
- * read_textfile- Read text file print to STDOUT.
- * @filename: text file being read
- * @letters: number of letters to be read
- * Return: w- actual number of bytes read and printed
- *        0 when function fails or filename is NULL.
- */
+ * read_textfile - read a text file and print to POSIX stdout
+ * @filename: name of the file to read from.
+ * @letters: number of letters to read and print
+ * Return: actual num of letters it could read and print.
+*/
+
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buf;
+	/*declare a pointer variable*/
 	ssize_t fd;
-	ssize_t w;
-	ssize_t t;
+	ssize_t writeBytes;
+	ssize_t readBytes;
+	char *buffer;
 
+	/*create and open the file using open*/
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-	buf = malloc(sizeof(char) * letters);
-	t = read(fd, buf, letters);
-	w = write(STDOUT_FILENO, buf, t);
+	/*allocate memory to store content of the file*/
+	buffer = malloc(sizeof(char) * letters);
+	/*Read from the file into the buffer*/
+	readBytes = read(fd, buffer, letters);
+	if (readBytes <= 0)
+	{
+		close(fd);
+		free(buffer);
+		return (0);
+	}
+	/*print content of buffer that was read to the stdout*/
+	writeBytes = write(STDOUT_FILENO, buffer, readBytes);
 
-	free(buf);
+	free(buffer);
 	close(fd);
-	return (w);
+	return (writeBytes);
 }
